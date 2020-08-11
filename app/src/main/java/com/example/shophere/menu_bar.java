9435c83416fdff6ce9eccf633cf4fdbe8cd562d1
liewmh0077 <liewmh-wm19@student.tarc.edu.my>
2020-08-11 15:37:34 +0800

    commit by minhui

---
+++ app/src/main/java/com/example/shophere/menu_bar.java
@@ -0,0 +1,14 @@
+package com.example.shophere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_bar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_bar);
+    }
+}
