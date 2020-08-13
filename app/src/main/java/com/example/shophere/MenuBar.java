f2ddaadeae058644f528d956c983b9cf9ecbd504
Lein Yi Shun <eason7716@gmail.com>
2020-08-13 19:49:46 +0800

    commit by Lein Yi Shun

---
+++ app/src/main/java/com/example/shophere/MenuBar.java
@@ -0,0 +1,14 @@
+package com.example.shophere;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuBar extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_bar);
+    }
+}
